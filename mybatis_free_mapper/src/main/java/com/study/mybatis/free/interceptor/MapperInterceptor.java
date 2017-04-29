57fea677277b09eb8be07daf63c8eec0e0317426
Galy <piguanghua365@163.com>
2017-04-30 00:50:09 +0800

    手动 还原

--- mybatis_free_mapper/src/main/java/com/study/mybatis/free/interceptor/MapperInterceptor.java
+++ mybatis_free_mapper/src/main/java/com/study/mybatis/free/interceptor/MapperInterceptor.java
@@ -24,8 +24,7 @@
 
 package com.study.mybatis.free.interceptor;
 
-import com.study.mybatis.free.free.MapperHelper;
-import org.apache.ibatis.builder.annotation.ProviderSqlSource;
+
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
@@ -46,7 +45,6 @@ import java.util.Properties;
 })
 public class MapperInterceptor implements Interceptor {
 
-    private final MapperHelper mapperHelper = new MapperHelper();
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
@@ -54,19 +52,8 @@ public class MapperInterceptor implements Interceptor {
         MappedStatement ms = (MappedStatement) objects[0];
         String msId = ms.getId();
         //不需要拦截的方法直接返回
-        if (mapperHelper.isMapperMethod(msId)) {
-            //第一次经过处理后，就不会是ProviderSqlSource了，一开始高并发时可能会执行多次，但不影响。以后就不会在执行了
-            if (ms.getSqlSource() instanceof ProviderSqlSource) {
-                mapperHelper.setSqlSource(ms);
-            }
-        }
-        Object result = invocation.proceed();
-        //是否对Map类型的实体处理返回结果，例如USER_NAME=>userName
-        //这个处理使得通用Mapper可以支持Map类型的实体（实体中的字段必须按常规方式定义，否则无法反射获得列）
-        if (mapperHelper.isCameHumpMap()) {
-            mapperHelper.cameHumpMap(result, ms);
-        }
-        return result;
+
+        return invocation.proceed();
     }
 
     @Override
@@ -80,39 +67,6 @@ public class MapperInterceptor implements Interceptor {
 
     @Override
     public void setProperties(Properties properties) {
-        String UUID = properties.getProperty("UUID");
-        if (UUID != null && UUID.length() > 0) {
-            mapperHelper.setUUID(UUID);
-        }
-        String IDENTITY = properties.getProperty("IDENTITY");
-        if (IDENTITY != null && IDENTITY.length() > 0) {
-            mapperHelper.setIDENTITY(IDENTITY);
-        }
-        String seqFormat = properties.getProperty("seqFormat");
-        if (seqFormat != null && seqFormat.length() > 0) {
-            mapperHelper.setSeqFormat(seqFormat);
-        }
-        String ORDER = properties.getProperty("ORDER");
-        if (ORDER != null && ORDER.length() > 0) {
-            mapperHelper.setBEFORE(ORDER);
-        }
-        String cameHumpMap = properties.getProperty("cameHumpMap");
-        if (cameHumpMap != null && cameHumpMap.length() > 0) {
-            mapperHelper.setCameHumpMap(cameHumpMap);
-        }
-        int mapperCount = 0;
-        String mapper = properties.getProperty("mappers");
-        if (mapper != null && mapper.length() > 0) {
-            String[] mappers = mapper.split(",");
-            for (String mapperClass : mappers) {
-                if (mapperClass.length() > 0) {
-                    mapperHelper.registerMapper(mapperClass);
-                    mapperCount++;
-                }
-            }
-        }
-        if (mapperCount == 0) {
-            throw new RuntimeException("通用Mapper没有配置任何有效的通用接口!");
-        }
+
     }
 }
