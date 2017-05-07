1e74249a12a1a9fdb6fbe9f041a9fe65e58fa936
Galy <piguanghua365@163.com>
2017-05-07 13:48:37 +0800

    同步

---
+++ mybatis_free_mapper/src/main/java/com/study/mybatis/free/helper/MapperHelper.java
@@ -0,0 +1,8 @@
+package com.study.mybatis.free.helper;
+
+/**
+ * Created by piguanghua on 2017/5/7.
+ */
+public class MapperHelper  {
+    public static final String DYNAMIC_SQL = "dynamicSQL";
+}
